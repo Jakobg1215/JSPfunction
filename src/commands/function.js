325fb6870020b4e74edc4638500309da77c1dd57
Jakob <74737820+Jakobg1215@users.noreply.github.com>
2020-12-31 22:55:56 -0600

    Update function.js

--- src/commands/function.js
+++ src/commands/function.js
@@ -8,23 +8,25 @@ module.exports = class sendPacket extends Plugin {
                 id: "functions:function",
                 description: "Runs commands in a function file",
                 execute: (sender, args) => {
-                    if (fs.existsSync(`./functions/${args[0]}.mcfunction`)) {
-                        const lineReader = require('readline').createInterface({
-                            input: fs.createReadStream(`./functions/${args[0]}.mcfunction`)
-                        });
-                        lineReader.on('line', function (line) {
-                            const commandManager = sender.getServer().getCommandManager();
-                            const Arguments = line.slice(0).split(/ +/);
-                            const Command = Arguments.shift().toLocaleLowerCase();
-                            // Thank you John.
-                            for (const command of commandManager.getCommands()) {
-                                const cmdname = command.id.split(":").pop();
-                                if (cmdname === Command) {
-                                    command.execute(sender, Arguments);
+                    if (args[0]) {
+                        if (fs.existsSync(`./functions/${args[0]}.mcfunction`)) {
+                            const lineReader = require('readline').createInterface({
+                                input: fs.createReadStream(`./functions/${args[0]}.mcfunction`),
+                            });
+                            lineReader.on('line', function (line) {
+                                const commandManager = sender.getServer().getCommandManager();
+                                const Arguments = line.slice(0).split(/ +/);
+                                const Command = Arguments.shift().toLocaleLowerCase();
+                                // Thank you John.
+                                for (const command of commandManager.getCommands()) {
+                                    const cmdname = command.id.split(":").pop();
+                                    if (cmdname === Command) {
+                                        command.execute(sender, Arguments);
+                                    };
                                 };
-                            };
-                        });
-                    } else return sender.sendMessage(`§c'${args[0]}' function doesn't exist!`)
+                            });
+                        } else return sender.sendMessage(`§c'${args[0]}' function doesn't exist!`);
+                    } else return sender.sendMessage("§cPlease put a function name in!");
                 },
             },
             this.getApi().getServer(),
