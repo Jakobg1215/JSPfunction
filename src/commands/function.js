eb505af8c5050e2fa7d09ea200cd7e87d97c4508
Jakob <74737820+Jakobg1215@users.noreply.github.com>
2021-01-08 15:19:12 -0600

    Update function.js

--- src/commands/function.js
+++ src/commands/function.js
@@ -1,32 +1,42 @@
 const Plugin = require("../base/BasePlugin");
 const fs = require("fs");
+const { literal, argument, string, } = require("@jsprismarine/brigadier");
 module.exports = class sendPacket extends Plugin {
     constructor(pluginData) {
         super(pluginData);
         this.getApi().getServer().getCommandManager().registerClassCommand(
             {
                 id: "functions:function",
-                description: "Runs commands in a functions file",
-                execute: (sender, args) => {
-                    if (args[0]) {
-                        if (fs.existsSync(`./functions/${args[0]}.mcfunction`)) {
-                            const lineReader = require('readline').createInterface({
-                                input: fs.createReadStream(`./functions/${args[0]}.mcfunction`),
-                            });
-                            lineReader.on('line', function (line) {
-                                const commandManager = sender.getServer().getCommandManager();
-                                const Arguments = line.slice(0).split(/ +/);
-                                const Command = Arguments.shift().toLocaleLowerCase();
-                                // Thank you John.
-                                for (const command of commandManager.getCommands()) {
-                                    const cmdname = command.id.split(":").pop();
-                                    if (cmdname === Command) {
-                                        command.execute(sender, Arguments);
-                                    };
-                                };
-                            });
-                        } else return sender.sendMessage(`§c'${args[0]}' function doesn't exist!`);
-                    } else return sender.sendMessage("§cPlease put a function name in!");
+                description: "Runs commands in a function file",
+                register: Dispatcher => {
+                    Dispatcher.register(
+                        literal("function").then(
+                            argument("function", string()).executes(
+                                async context => {
+                                    const infunction = context.getArgument("function");
+                                    const sender = context.getSource();
+                                    if (infunction) {
+                                        if (fs.existsSync(`./functions/${infunction}.mcfunction`)) {
+                                            const lineReader = require('readline').createInterface({
+                                                input: fs.createReadStream(`./functions/${infunction}.mcfunction`),
+                                            });
+                                            lineReader.on('line', function (line) {
+                                                const commandManager = sender.getServer().getCommandManager();
+                                                const incommand = line.slice(0).split(/ +/).shift().toLocaleLowerCase();
+                                                // Thank you John.
+                                                for (const command of commandManager.getCommandsList()) {
+                                                    if (command[0] === incommand) {
+                                                        commandManager.dispatchCommand(sender, line);
+                                                        break;
+                                                    };
+                                                };
+                                            });
+                                        } else return sender.sendMessage(`§c'${infunction}' function doesn't exist!`);
+                                    } else return sender.sendMessage("§cPlease put a function name in!");
+                                },
+                            ),
+                        ),
+                    );
                 },
             },
             this.getApi().getServer(),
