164304bdee5c7ff431a8d0e31abb8df8cb39297d
Jakob <74737820+Jakobg1215@users.noreply.github.com>
2021-01-08 15:19:27 -0600

    Update listfunction.js

--- src/commands/listfunction.js
+++ src/commands/listfunction.js
@@ -1,16 +1,23 @@
 //Thank you Filiph Sandström for the idea!
 const Plugin = require("../base/BasePlugin");
 const fs = require("fs");
+const { literal, } = require("@jsprismarine/brigadier");
 module.exports = class sendPacket extends Plugin {
     constructor(pluginData) {
         super(pluginData);
         this.getApi().getServer().getCommandManager().registerClassCommand(
             {
                 id: "functions:listfunction",
-                description: "Lists all functions in the functions folder!",
-                execute: (sender, args) => {
-                    const functionFiles = fs.readdirSync("./functions").filter(file => file.endsWith(".mcfunction"));
-                    return sender.sendMessage(`${functionFiles}`);
+                description: "Lists all functions in the function folder!",
+                register: Dispatcher => {
+                    Dispatcher.register(
+                        literal("listfunction").executes(
+                            context => {
+                                const functionFiles = fs.readdirSync("./functions").filter(file => file.endsWith(".mcfunction"));
+                                return context.getSource().sendMessage(`${functionFiles}`);
+                            },
+                        ),
+                    );
                 },
             },
             this.getApi().getServer(),
